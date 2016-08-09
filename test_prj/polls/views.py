8f51b00631adc7a03ce554271f999eeac6e41466
gmth7788 <orchard2046@163.com>
2016-08-09 17:19:21 +0800

    通过以下地址访问 http://localhost:8000/polls/

--- test_prj/polls/views.py
+++ test_prj/polls/views.py
@@ -1,3 +1,12 @@
+#coding=utf-8
+
 from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("hello, world. Yor're at the polls index."
+                        "<p>嗨，我在这里</p>")
+
 
 # Create your views here.
